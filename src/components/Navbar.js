84ac6213b5f842ecf003caff7166f569e0324dc8
Chloe Moss <chloe@chloeintech.com>
2023-08-31 09:19:04 +0100

    home mobile updates

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,49 +1,56 @@
-import React, {useState} from 'react';
-import { Link } from 'react-router-dom';
-import * as FaIcons from "react-icons/fa"
-import * as AiIcons from "react-icons/ai"
-import { Sidebardata } from './Sidebardata';
-import "./Navbar.css"
-import { IconContext } from 'react-icons';
-
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
+import { Sidebardata } from "./Sidebardata";
+import "./Navbar.css";
+import { IconContext } from "react-icons";
+import LinkedIn from "../components/images/linkedin.png";
+import Github from "../components/images/github.png";
+import Email from "../components/images/email.png";
 
 function Navbar() {
-
-    const [sidebar, setSidebar] = useState(false);
-    const showSidebar = () => setSidebar(!sidebar);
+  const [sidebar, setSidebar] = useState(false);
+  const showSidebar = () => setSidebar(!sidebar);
 
   return (
     <>
-    {/* <IconContext.Provider value={{ color : '#FF9045'}}> */}
-    <div className='navbar'>
-        <Link to="#" className='menu-bars'>
-            <FaIcons.FaBars onClick={showSidebar}/>
+      {/* <IconContext.Provider value={{ color : '#FF9045'}}> */}
+      <div className="navbar">
+
+        <Link to="#" id="cssnav" className="navleft">
+          <FaIcons.FaBars onClick={showSidebar} />
         </Link>
 
-        
-    </div>
-    <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
-        <ul className='nav-menu-items'onClick={showSidebar} >
-            <li className='navbar-toggle'>
-                <Link to="#" className='menu-bars'>
-                    <AiIcons.AiOutlineClose  />
-                </Link>
-            </li>
-            {Sidebardata.map((item, index,) => {
-                return(
-                    <li key={index} className={item.cName}>
-                        <Link to={item.path}>
-                            {item.icon}
-                            <span>{item.title}</span>
-                        </Link>
-                    </li>
-                )
-            })}
+        <ul id="cssnav" className="navright">
+          <img className="social-image" src={LinkedIn} alt="linkedin"></img>
+          <img className="social-image" src={Github} alt="github"></img>
+          <img className="social-image" src={Email} alt="email"></img>
         </ul>
-    </nav>
-    {/* </IconContext.Provider> */}
+
+      </div>
+      <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
+        <ul className="nav-menu-items" onClick={showSidebar}>
+          <li className="navbar-toggle">
+            <Link to="#" className="x-bars">
+              <AiIcons.AiOutlineClose />
+            </Link>
+          </li>
+          {Sidebardata.map((item, index) => {
+            return (
+              <li key={index} className={item.cName}>
+                <Link to={item.path}>
+                  {item.icon}
+                  <span>{item.title}</span>
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+      {/* </IconContext.Provider> */}
     </>
-  )
+  );
 }
 
 export default Navbar;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,25 +1,24 @@
-import React, { useRef, useEffect } from "react";
+import React from "react";
 import "./Home.css";
 import About from "../components/images/about.png";
 import Portfolio from "../components/images/portfolio.png";
 import CV from "../components/images/cv.png";
 import Contact from "../components/images/contact.png";
-import LinkedIn from "../components/images/linkedin.png";
-import Github from "../components/images/github.png";
-import Email from "../components/images/email.png";
+
 
 const Home = () => {
 
   return (
-    <div className="home">
+    <>
+
+      <div className="home-container">
+
+      <div className="chloe-container">
+      Chloe Moss
+      </div>
 
       <div className="portfolio-container">
-        <div className="chloe">
-          <h4>Chloe Moss</h4>
-        </div>
-        <div className="portfolio">
-          <h3>Portfolio</h3>
-        </div>
+      Portfolio
       </div>
 
       <div className="grid-container">
@@ -29,20 +28,19 @@ const Home = () => {
         <img className="grid-image1" src={Contact} alt="contact"  onClick={event=> window.location.href='/contact'}></img>
       </div>
 
+      </div> 
+
       <div className="text-container">
-        <h1>
-          <i>Crafting code with a </i><b>love for creativity.</b> <br  />
-          <i> I'm a junior software developer who </i> 
-          <b>colours outside the lines of convention.</b>
-        </h1>
+          Crafting code with a love for creativity.<br  />
+          I'm a junior software developer who colours outside the lines of convention.
       </div>
 
-      <div className="social-container">
+      {/* <div className="social-container">
         <img className="social-image" src={LinkedIn} alt="linkedin"></img>
         <img className="social-image" src={Github} alt="github"></img>
         <img className="social-image" src={Email} alt="email"></img>
-      </div>
-    </div>  );
+      </div> */}
+    </>  );
 };
 
 export default Home;
